a58cae2b4c5f5ce3db508c2df46de2a5dc44ccef
sinsehwan <sk124590@gmail.com>
2025-02-18 01:47:34 +0900

    chatting draft

--- triton/src/main/java/com/example/triton/chatting/Entity/ChatMessage.java
+++ triton/src/main/java/com/example/triton/chatting/Entity/ChatMessage.java
@@ -18,6 +18,6 @@ public class ChatMessage {
     private Long mid;
     private String message;
     private Long senderId;
-    private Long cid;
+    private Long chatId;
     private Date mtime;
 }
