0c629216fa91cc198c0d509ace2cddba6f0fe9a1
Whitedoggy <nubuli19@gmail.com>
2025-02-17 15:45:48 +0900

    로그인 기능 구현

---
+++ triton/src/main/java/com/example/triton/user/UserService.java
@@ -0,0 +1,25 @@
+package com.example.triton.user;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public SiteUser create(String username, String usertype, String password) {
+        SiteUser user = new SiteUser();
+        user.setUsername(username);
+        user.setPassword(passwordEncoder.encode(password));
+        user.setUsertype(usertype);
+        user.setPhotoUrl("");
+        this.userRepository.save(user);
+        return user;
+    }
+}
