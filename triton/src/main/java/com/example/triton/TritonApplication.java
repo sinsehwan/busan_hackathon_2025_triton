a803d0be35a37e12002f7c17c4121740361baf16
sinsehwan <sk124590@gmail.com>
2025-02-17 10:36:10 +0900

    init setting

---
+++ triton/src/main/java/com/example/triton/TritonApplication.java
@@ -0,0 +1,13 @@
+package com.example.triton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TritonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TritonApplication.class, args);
+	}
+
+}
---
+++ triton/src/test/java/com/example/triton/TritonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.triton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TritonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
